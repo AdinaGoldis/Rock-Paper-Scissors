76109b332c3d0eaf67f13715f2c5de6ced69c820
“AdinaGoldis” <agoldis29@gmail.com>
2022-08-08 19:56:55 +0200

    HTML 1.0

---
+++ main.js
@@ -0,0 +1,59 @@
+console.log("test");
+
+// einlesen der Rundenauswahl
+let maxGames = 5; // ============= vorläufig auf 15 setzen
+
+// function gamesNumber() {
+//     if (val1 = true) {
+//         maxGames = 5;
+//     }
+//     else {
+//         if (val2 = true) {
+//             maxGames = 10;
+//         }
+//         else {
+//             if (val3 = true) {
+//                 maxGames = 15;
+//             }
+//             else {
+//                 if (val4 = true) {
+//                     maxGames = 20;
+//                 }
+//                 else {
+//                     maxGames = "choose";
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// console.log(gamesNumber);
+
+//==================einlesen der Symbolauswahl
+
+let yourChoice = 0;
+let auswahlZahl;
+let computersChoice = 0;
+let resultYou = 0;
+let resultComp = 0;
+
+function addRock() {
+    yourChoice.innerText = "Your choice is: rock";
+    // let computersChoice = Math.floor(Math.random() * 4);
+    // let rock = 0;
+    
+    //if(computersChoice === rock)
+}
+
+
+
+
+
+
+score.innerText = "The game ends:"; //test
+
+// Spiel: Computer
+
+// Auswertung
+
+// Anzeige udn Hochzählen
